bdf5d65acbf4a5972f628d2f38097aabd33ff92c
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-05-01 22:27:13 +0530

    Add files via upload

--- census.py
+++ census.py
@@ -77,7 +77,7 @@ if uploaded_file is not None:
     if st.header("\nData visualizations"):
         if st.checkbox("Show the percentages of Religions in India by a piechart"):
             st.write()
-            fig = plt.figure(figsize=(20,10))
+            fig = plt.figure(figsize=(24,12))
             ax1 = plt.subplot(312)
             explode = (0.1, 0.1, 0.1, 0.1,0.1,0)
             labels = ['Sikhs', 'Christians', 'Jains', 'Buddhists','Hindus','Muslims']
