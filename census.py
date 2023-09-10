36062d839aea65c80ab26607c68dadd35ed2a36b
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-11 00:06:43 +0530

    Update census.py

--- census.py
+++ census.py
@@ -3,18 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import plotly.express as px
+import plotly.express as px 
+import random
 from PIL import Image
-
-st.title("Exploratory Data Analysis on India Census Data Set")
-st.image('logo.png')
-
-# Load the dataset
-data = pd.read_csv('6. India Census 2011.csv')  # Replace 'your_dataset.csv' with the actual file path of your dataset
 logo = Image.open('logo.png')
-
-# The rest of your code remains the same
-st.set_page_config(page_title="INDIA CENSUS EDA", page_icon=":bar_chart:", layout="wide")
+#pip install pandas numpy matplotlib seaborn streamlit
+#to run streamlit :   streamlit run census.py 
+st.set_page_config(page_title="INDIA CENSUS  EDA", page_icon=":bar_chart:", layout="wide")
 st.image(logo)
 # Define the list of names
 names = ["21A21A6111-E Jeji Anil", "21A21A6158-Tusha Rahul B ", "21A21A6137-M S R Chandrika","21A21A6166-K Shyam chand","21A21A6101-A Leena","21A21A6140-N Upendra","21A21A6157-T Sumanth Raju","22A25A6105(L5)-T Naveen Babu"]
@@ -27,9 +22,7 @@ for name in names:
 st.sidebar.title("Under The Guidance of :")
 st.sidebar.write("Dr.Bomma.Ramakrishna")
 # File upload
-uploaded_file = st.file_uploader("Choose a India Census Dataset csv")
-if uploaded_file is not None:
-    data=pd.read_csv(uploaded_file)
+    data=pd.read_csv('6. India Census 2011.csv')
     st.dataframe(data)
     
     st.title("India Census Data Analysis")
